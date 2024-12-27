1d23c07448f2d60ab8367c464a73054519b25143
Nur Mahammad Mondol Robiul <nurmahammadmondolrobiul@gmail.com>
2024-12-26 21:13:44 -0400

    Create a new web page Restaurant Management client side

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-emerald-500">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Router/Routers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const Routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world web page create success!</div>,
+  },
+]);
+
+export default Routers;
